48ea820bfce07f66a0fe6383630c3d49708c8364
Traci Goreham <gorehamt@oregonstate.edu>
2019-04-14 20:22:23 -0500

    fix to smithyFunc

--- projects/gorehamt/dominion/dominion.c
+++ projects/gorehamt/dominion/dominion.c
@@ -861,16 +861,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case smithy:
-    /*
-      //+3 Cards
-      for (i = 0; i < 3; i++){
-	      drawCard(currentPlayer, state);
-	    }
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
-      */
       return smithyFunc(state, currentPlayer, handPos);
 		
     case village:
