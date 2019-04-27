95f0ecba7935d2d19d4bc3472c44535a67282d7a
Traci Goreham <gorehamt@oregonstate.edu>
2019-04-27 08:31:34 -0500

    added last unit test for unittest.c, also added error to get a fail example

--- projects/gorehamt/dominion/unittest1.c
+++ projects/gorehamt/dominion/unittest1.c
@@ -22,7 +22,7 @@ int main(){
 
     initializeGame(numPlayers, kingdomCards, seed, &state);
 
-    printf("------Testing isGameOver function------\n");
+    printf("\n------Testing isGameOver function------\n");
 
     /*Test 1: Game is over because province supply is 0. */
     state.supplyCount[province] = 0; 
@@ -50,11 +50,11 @@ int main(){
 
     /*Test 3: Game is not over because provice supply > 0 and < 3 supply piles have 0 cards */
     state.supplyCount[province] = 5;
-    state.supplyCount[7] = 4; //adventurer
+    state.supplyCount[7] = 0; //adventurer
     state.supplyCount[17] = 0; //minion
     state.supplyCount[22] = 0; //embargo
 
-    printf("\nTesting when province supply count is > 0 and three supply piles are not equal 0.\n");
+    printf("\nTesting when province supply count is > 0 and three supply piles are not equal to 0.\n");
     if ((isGameOver(&state)) == 0){
         printf("\tTEST SUCCESSFULLY COMPLETED\n");
     }
@@ -62,6 +62,19 @@ int main(){
         printf("\tTEST FAILED: Game was over even though province supply count was > 0 and three supply piles were not equal to 0.\n");
     }
 
+    /*Test 4: Game is over because provice supply = 0 and 3 supply piles have 0 cards */
+    state.supplyCount[province] = 0;
+    state.supplyCount[7] = 0; //adventurer
+    state.supplyCount[17] = 0; //minion
+    state.supplyCount[22] = 0; //embargo
+
+    printf("\nTesting when province supply count is = 0 and three supply piles are equal to 0.\n");
+    if ((isGameOver(&state)) == 1){
+        printf("\tTEST SUCCESSFULLY COMPLETED\n");
+    }
+    else{
+        printf("\tTEST FAILED: Game was not over even though province supply count was = 0 and three supply piles were equal to 0.\n");
+    }
 
     return 0;
 }
