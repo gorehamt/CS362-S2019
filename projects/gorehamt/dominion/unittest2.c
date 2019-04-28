e8aa9e263cee9669178cfc3b0b75179b06ecade8
Traci Goreham <gorehamt@oregonstate.edu>
2019-04-28 10:25:50 -0500

    added more tests to unittest2

--- projects/gorehamt/dominion/unittest2.c
+++ projects/gorehamt/dominion/unittest2.c
@@ -28,48 +28,49 @@ int main(){
     int bonus = -1;
     state.hand[currentPlayer][handPos] = council_room;
 
-    //get current hand size, number of buys
-    int p1HandSizeBefore = state.handCount[currentPlayer];
-    int p1NumBuysBefore = state.numBuys;
+    //get current hand size, number of buys, etc.
+    int currentPHandSizeBefore = state.handCount[currentPlayer];
+    int currentPNumBuysBefore = state.numBuys;
+    int otherPHandSizeBefore = state.handCount[1];
 
     //play council_room card
     cardEffect(council_room, -1, -1, -1, &state, handPos, &bonus);
 
-    //get hand size, num of buys after card is played
-    int p1HandSizeAfter = state.handCount[currentPlayer];
-    int p1NumBuysAfter = state.numBuys;
+    //get hand size, num of buys, etc. after card is played
+    int currentPHandSizeAfter = state.handCount[currentPlayer];
+    int currentPNumBuysAfter = state.numBuys;
+    int otherPHandSizeAfter = state.handCount[1];
 
     printf("\n------Testing Council Room Card Implementation------\n");
 
     /*Test 1: Verify that the current player receives exactly four cards.*/
 
     printf("\nTesting that current player's hand size increases by exactly 4 cards after council room card is played.\n");
-    if (p1HandSizeAfter == p1HandSizeBefore + 4){
+    if (currentPHandSizeAfter == currentPHandSizeBefore + 3){ //3 because of discard.
         printf("\tTEST SUCCESSFULLY COMPLETED\n");
     }
     else{
-        printf("\tTEST FAILED: Hand size before was % d, and hand size after was % d\n", p1HandSizeBefore, p1HandSizeAfter);
+        printf("\tTEST FAILED: Hand size before was %d, and hand size after was %d\n", currentPHandSizeBefore, currentPHandSizeAfter);
     }
 
     /*Test 2: Verify that the current player receives exactly one more buy*/
     printf("\nTesting that current player's number of buys increases by exactly 1 after council room card is played.\n");
-    if (p1NumBuysAfter == p1NumBuysBefore + 1){
+    if (currentPNumBuysAfter == currentPNumBuysBefore + 1){
         printf("\tTEST SUCCESSFULLY COMPLETED\n");
     }
     else{
-        printf("\tTEST FAILED: Number of buys before was % d, and number of buys after was % d\n", p1NumBuysBefore, p1NumBuysAfter);
+        printf("\tTEST FAILED: Number of buys before was %d, and number of buys after was %d\n", currentPNumBuysBefore, currentPNumBuysAfter);
     }
-
 
     /*Test 3: Verify that the other players receives exactly one more card*/
- /*   printf("\nTesting that other player's number of cards increases by exactly 1 after council room card is played.\n");
-    if (p1NumBuysAfter == p1NumBuysBefore + 1){
+    printf("\nTesting that the other player's number of cards increases by exactly 1 after council room card is played.\n");
+    if (otherPHandSizeAfter == otherPHandSizeBefore + 1){
         printf("\tTEST SUCCESSFULLY COMPLETED\n");
     }
     else{
-        printf("\tTEST FAILED: Number of buys before was % d, and number of buys after was % d\n", p1NumBuysBefore, p1NumBuysAfter);
+        printf("\tTEST FAILED: Number of cards in hand before was %d, and number of cards in hand after was %d\n", otherPHandSizeBefore, otherPHandSizeAfter);
     }
-*/
+
     return 0;
 }
 
