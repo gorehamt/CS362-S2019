7a49534ab9ac704996121d9d8ef4bd94c651947c
Traci Goreham <gorehamt@oregonstate.edu>
2019-05-15 19:20:52 -0500

    remove print statements and up the number of times the random test runs

--- projects/gorehamt/dominion/randomtestadventurer.c
+++ projects/gorehamt/dominion/randomtestadventurer.c
@@ -27,7 +27,6 @@
 
 void testAdventurer(struct gameState *post){
 
-    printf ("Got here: A\n");
     //create a struct to save the gamestate prior to the cardEffect function being called
     //and copy the current gamestate into that struct
     struct gameState pre;
@@ -39,18 +38,11 @@ void testAdventurer(struct gameState *post){
     int cardEffectResult;
     int player = post->whoseTurn;
 
-    printf ("Got here: B\n");
-
     //call card effect function with the adventurer card using the randomly generated gamestate
     cardEffectResult = cardEffect(adventurer, -1, -1, -1, post, handPos, &bonus); 
 
-    printf ("Got here: C\n");
-    printf ("post->handcount is %d and pre.handCount is %d\n", post->handCount[player], pre.handCount[player]);
-
     //check that the adventurer card was implemented correctly by comparing the pre and post states
     assert(post->handCount[player] == pre.handCount[player] + 2); //two treasure cards were added to the handcount
-
-    printf ("Got here: D\n");
 
     //check that the cardEffect function returned without an error
     assert(cardEffectResult == 0);
@@ -74,7 +66,7 @@ int main(int argc, char *argv[]){
 
     int n; //number of times to run the random tester
     int i;
-    for (n = 0; n < 5; n++) { //TRACI--change this to a larger number later
+    for (n = 0; n < 2000; n++) { //TRACI--change this to a larger number later
         //fill the gameState with randomly generated input
         for (i = 0; i < sizeof(struct gameState); i++) {
             ((char*)&G)[i] = floor(Random() * 256);
@@ -82,7 +74,6 @@ int main(int argc, char *argv[]){
         player = floor(Random() * 2);
         G.whoseTurn = player;
         G.deckCount[player] = floor(Random() * (MAX_DECK - 8)) + 8; //to ensure room for 7 coppers
-        printf("Deck Count is %d\n", G.deckCount[player]);
 
         //need to add 7 coppers to the player's deck to work with the way adventurer code is written
         int k, j;
